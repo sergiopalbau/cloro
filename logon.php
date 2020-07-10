25c37369dd9782adfbf556df85cb9c1bb6703ac5
sergio <s.palomo.b@gmail.com>
2020-07-10 10:22:53 +0200

    login web
    PRIMERAS PRUEBAS

---
+++ logon.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+$user = "sanandres";
+$pwd= "sanandres";
+
+
+if ($_POST) {
+	if (empty($_POST['usuario']) || empty($_POST['pwd']) || !isset($_POST['usuario']) || !isset($_POST['pwd'])){
+		$msg = "datos incorrectos";
+		$_SESSION['msg'] = $msg;
+		header('Location:login.php');
+		die();
+
+	}else{
+		if ($user == $_POST['usuario'] && $pwd == $_POST['pwd']){
+			$_SESSION['login']=true;
+		}
+		
+	}
+}
+
+if(!isset($_SESSION['login'])) {
+		$msg = "Ingrese credenciales";
+		$_SESSION['msg'] = $msg;
+		var_dump($msg);
+		header('Location:login.php');
+
+	}
+
+header('Location:index.php');
+?>
+
