787f0997d7575c4ad1adda6b3758ba5eb5eddd63
sergio <s.palomo.b@gmail.com>
2020-07-12 08:20:14 +0200

    primeros pasos partes comunes del site

---
+++ pages/head.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>304SAR</title>
+	<link rel="stylesheet" href="css/picnic.min.css">
+	<link rel="stylesheet" href="css/nav.css">
+</head>
+
+<body>
---
+++ pages/nav.php
@@ -0,0 +1,31 @@
+
+
+<div class="flex center">
+	
+	<nav class="">
+
+		<div class="brand brand_margin ">
+  			<a href="#" class="">
+       			<span>304-SAR</span>
+ 			</a>
+ 		</div>
+
+  		<!-- responsive-->
+  		<input id="bmenub" type="checkbox" class="show">
+ 	 	<label for="bmenub" class="burger pseudo button">  &#9776; </label>
+
+		<div class="menu">
+		  <a href="cloro.php" class="pseudo button">control Cloro</a>
+		 <!--Aqui van los links a las siguientes enlaces -->
+		</div>
+	</nav>
+</div>
+<br><br>
+	
+
+
+<!-- 
+
+
+x
+-->
